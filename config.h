8e703f9801c056d706d2c196528492053c5ce99d
Ashley Roll <ash@digitalnemesis.com>
2021-03-30 19:51:12 +1000

    Make gaps consistent

--- config.h
+++ config.h
@@ -8,10 +8,10 @@
 /* appearance */
 static unsigned int borderpx  = 3;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
-static unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
@@ -50,11 +50,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	*/
 	/* class        instance        title               tags mask    isfloating   isterminal  noswallow  monitor */
-	{ "Gimp",       NULL,           NULL,       	    1 << 8,       0,           0,         0,         -1 },
+	{ "Gimp",       NULL,           NULL,       	    1 << 8,       0,           0,         0,         -1 },  // tag 9
 	{ TERMCLASS,    NULL,           NULL,       	    0,            0,           1,         0,         -1 },
 	{ NULL,         NULL,           "Event Tester",     0,            0,           0,         1,         -1 },
 	{ NULL,         "spterm",       NULL,       	    SPTAG(0),     1,           1,         0,         -1 },
 	{ NULL,         "spcalc",       NULL,     	        SPTAG(1),     1,           1,         0,         -1 },
+    // Chat
     { "discord",    "discord",      NULL,               1,            0,           0,         0,          0 },   // second monitor tag 1
     { "Slack",      "slack",        NULL,               1,            0,           0,         0,          0 },   // second monitor tag 1
     { NULL,         NULL,           "WhatsApp",         1 << 1,       0,           0,         0,          0 },   // second monitor tag 2
@@ -65,7 +66,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
